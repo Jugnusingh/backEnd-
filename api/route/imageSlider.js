d31df28adc808f1b41eaf6cf4ae7ad8bc8fb3ed3
jugnu <jugnu908@gmail.com>
2023-07-25 19:51:50 +0530

    add backend code for image slider

--- api/route/imageSlider.js
+++ api/route/imageSlider.js
@@ -1,39 +1,67 @@
-const express = require("express")
-const router = express.Router()
-const Image = require("../../schema/ImageSlider")
-router.get("/", async(req, res) => {
-    console.log(req,"dfads")
-    Image.find()
-        .then((result) => {
-            res.status(200).json({
-                imageData: result
-            })
-        }).catch((error) => {
-            console.log(error);
-            res.status(200).json({
-                error:error
-            })
-        })
-})
+// api/route/imageSlider.js
+
+const express = require("express");
+const router = express.Router();
+const Image = require("../../schema/ImageSlider");
+
+// GET all images
+router.get("/", async (req, res) => {
+  try {
+    const images = await Image.find();
+    res.status(200).json({
+      imageData: images,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: "Error fetching images.",
+    });
+  }
+});
+
+// POST a new image
 router.post("/", (req, res) => {
-
+    const { Title, Description} = req.body;
+  
     const image = new Image({
-        Title: req.body.Title,
-        Description: req.body.Description,
-        Image:req.body.ImgUrl
-    })
+      Title,
+      Description,
+    //   ImgUrl: req.files['Image'][0].filename,
+      
+    });
+  
     image.save()
-        .then((result) => {
-            console.log(result)
-            res.status(200).json({
-                newImg: result
-            })
-        }).catch((error) => {
-            console.log(error)
-            res.status(500).json({
-                error: error
-            })
-        })
-})
+      .then((result) => {
+        console.log(result);
+        res.status(200).json({
+          newImage: result,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).json({
+          error: "Failed to save product",
+        });
+      });
+  });
 
-module.exports = router
+// DELETE an image
+router.delete("/:imageId", async (req, res) => {
+  try {
+    const imageId = req.params.imageId;
+    const deletedImage = await Image.findByIdAndRemove(imageId);
+    if (!deletedImage) {
+      return res.status(404).json({ error: "Image not found" });
+    }
+    res.status(200).json({
+      deletedImage: deletedImage,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: "Error deleting image.",
+    });
+  }
+});
+
+module.exports = router;
