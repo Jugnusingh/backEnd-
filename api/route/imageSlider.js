59e79a28abd39a6c9ff4cae670518b6f57d5e43e
Jugnusingh <46666515+Jugnusingh@users.noreply.github.com>
2023-07-25 23:38:19 +0530

    Update imageSlider.js

--- api/route/imageSlider.js
+++ api/route/imageSlider.js
@@ -1,8 +1,21 @@
-// api/route/imageSlider.js
-
 const express = require("express");
 const router = express.Router();
 const Image = require("../../schema/ImageSlider");
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const fileExtension = path.extname(file.originalname);
+    cb(null, file.fieldname + "-" + uniqueSuffix + fileExtension);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 // GET all images
 router.get("/", async (req, res) => {
@@ -20,30 +33,20 @@ router.get("/", async (req, res) => {
 });
 
 // POST a new image
-router.post("/", (req, res) => {
-    const { Title, Description} = req.body;
-  
-    const image = new Image({
-      Title,
-      Description,
-    //   ImgUrl: req.files['Image'][0].filename,
-      
-    });
-  
-    image.save()
-      .then((result) => {
-        console.log(result);
-        res.status(200).json({
-          newImage: result,
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-        res.status(500).json({
-          error: "Failed to save product",
-        });
-      });
-  });
+router.post("/", upload.single("Image"), async (req, res) => {
+  try {
+    const { Title, Description } = req.body;
+    const ImgUrl = req.file ? req.file.filename : null; // Get the uploaded image filename if available
+
+    const newImage = new Image({ Title, Description, ImgUrl });
+    const savedImage = await newImage.save();
+
+    res.status(201).json(savedImage);
+  } catch (error) {
+    console.error("Error creating blog:", error);
+    res.status(500).json({ error: "Failed to create blog" });
+  }
+});
 
 // DELETE an image
 router.delete("/:imageId", async (req, res) => {
