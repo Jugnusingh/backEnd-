bf89e8a62ee909891ac9242692b76d0d40d9d052
jugnu <jugnu908@gmail.com>
2023-08-31 12:53:23 +0530

    Committing changes to paymentRouter.js

--- api/route/paymentRouter.js
+++ api/route/paymentRouter.js
@@ -4,4 +4,4 @@ const paymentController = require('../../controller/paymentController');
 
 // Define a route to create a payment intent
 router.post("/createCheckoutSession", paymentController.createCheckoutSession);
-module.exports = router; 
+module.exports = router;  
