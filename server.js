0273efc25cd8a1b1327e19b29bdae7c30b7707a6
Jugnusingh <46666515+Jugnusingh@users.noreply.github.com>
2024-01-11 21:07:52 +0530

    Update server.js
    we have intigrated ssl certificate in my code

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
-
 require("dotenv").config();
-const http = require("http");
+const https = require("https");
+const fs = require("fs");
 const app = require("./App");
 
 const IP_ADDRESS = process.env.IP; // Replace 'your_static_ip' with your actual static IP address
 const PORT = process.env.PORT;
 
-const server = http.createServer(app);
+// Paths to SSL certificate and private key files obtained from Certbot
+const certificatePath = "/etc/letsencrypt/live/waveart.in/fullchain.pem";
+const privateKeyPath = "/etc/letsencrypt/live/waveart.in/privkey.pem"; // Corrected the file name here
+const privateKey = fs.readFileSync(privateKeyPath, "utf8");
+const certificate = fs.readFileSync(certificatePath, "utf8");
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+};
+
+const server = https.createServer(credentials, app);
 
 server.listen(PORT, IP_ADDRESS, () => {
-    console.log(`Server is running at http://${IP_ADDRESS}:${PORT}/`);
+  console.log(`Server is running at https://${IP_ADDRESS}:${PORT}/`);
 });
-
