b9104f06b991cdf912b4ea4e1035c52d3d09f662
Jugnusingh <46666515+Jugnusingh@users.noreply.github.com>
2024-01-11 21:03:04 +0530

    Update orderSchema.js
    order schema has update as new requirment

--- schema/orderSchema.js
+++ schema/orderSchema.js
@@ -1,35 +1,16 @@
 const mongoose = require('mongoose');
+
 const orderSchema = new mongoose.Schema({
-  title:{
-    type:String,
-  },
-    productIds: {
-    type: String,
-  },
-  amount: {
-    type: Number,
-    required: true,
-  },
-  currency: {
-    type: String,
-    required: true,
-  },
-  receipt: {
-    type: String,
-    required: true,
-  },
-  razorpayOrderId: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    default: 'pending',
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+  userId: String,
+  order_id: String,
+  order_status: String,
+  tracking_id: String,
+  title: String,
+  productID: String,
+  name: String,
+  email: String,
+  phone: String,
+  createdAt: { type: Date, default: Date.now },
 });
 
 const Order = mongoose.model('Order', orderSchema);
